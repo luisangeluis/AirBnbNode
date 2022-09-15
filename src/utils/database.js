0c1a79c60a293562d4802c1a9875bd34d65e8e59
luisangeluis <luisangeluis@gmail.com>
2022-09-15 00:53:56 -0500

    Updated config database

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,11 +2,18 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '12345',
-  database: 'airbnb',
-  port: '5432'
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: process.env.PORT
+
+  // dialect: 'postgres',
+  // host: 'localhost',
+  // username: 'postgres',
+  // password: '12345',
+  // database: 'airbnb',
+  // port: '5432'
 })
 
 
