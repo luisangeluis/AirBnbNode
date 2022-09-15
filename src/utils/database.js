e0c00b30ac2ac0537c5cb8b40c9a87ac724b806c
luisangeluis <luisangeluis@gmail.com>
2022-09-15 01:11:01 -0500

    Configuring db

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,7 +6,7 @@ const db = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  port: process.env.PORT
+  port: '5432'
 
   // dialect: 'postgres',
   // host: 'localhost',
